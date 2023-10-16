ee2e5f15dc9165f55ad59f5574f527aa6bc82e03
Robert Cohn <robert.s.cohn@intel.com>
2023-10-16 09:33:52 -0700

    update

--- send-receive.cpp
+++ send-receive.cpp
@@ -22,8 +22,12 @@ int main(int argc, char *argv[]) {
   }
   
   MPI_Request send_request, recv_request;
-  MPI_Isend(data, 1, MPI_INT, (rank + 1) % size , 0, comm, &send_request);
-  MPI_Irecv(data, 1, MPI_INT, (rank - 1) % size, 0, comm, &recv_request);
+  int dst_rank = (rank + 1) % size;
+  int src_rank = (rank - 1 + size) % size;
+  std::cout << "rank: " << rank << " dest: " << dst_rank << " src: " << src_rank << "\n";
+  MPI_Irecv(data, 1, MPI_INT, src_rank, 0, comm, &recv_request);
+  MPI_Isend(data, 1, MPI_INT, dst_rank, 0, comm, &send_request);
+
   MPI_Wait(&send_request, MPI_STATUS_IGNORE);
   std::cout << "Send complete\n";
   MPI_Wait(&recv_request, MPI_STATUS_IGNORE);
